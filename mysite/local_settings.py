629f152deabe9c488155178bdcd8232bdf9ca13f
Aneta Włodarczyk <anetka.wlodarczyk@gmail.com>
2015-04-14 11:53:37 +0200

    Add missing forms and local settings files.

---
+++ mysite/local_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'postgres',
+        'PASSWORD': 'anetka9',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+DEBUG = True
